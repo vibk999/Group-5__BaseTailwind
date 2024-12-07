ba697907ab4ff7ebc082f23bcac32308bf24d1ba
qvu04 <quangvu221004@gmail.com>
2024-12-07 14:17:41 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  darkMode: ['selector', '[data-mode="dark"]'],
+  darkMode: "class",
   theme: {
     extend: {},
   },
